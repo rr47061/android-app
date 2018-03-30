fbc5e61b779501feecd065d678bef81b7d3b880e
rohit ranjan <ranjan.rohit64@gmail.com>
2018-03-31 00:18:22 +0530

    uploaded android files

---
+++ app/src/main/java/com/example/rohitranjan/logfiletest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.rohitranjan.logfiletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnClick(View v)
+    {
+        EditText e1=(EditText) findViewById(R.id.editText);
+        EditText e2=(EditText) findViewById(R.id.editText2);
+        EditText e3=(EditText)findViewById(R.id.editText3);
+        int num1 = Integer.parseInt(e1.getText().toString());
+        int num2 = Integer.parseInt(e2.getText().toString());
+        int sum = num1  + num2;
+        e3.setText(Integer.toString(sum));
+
+    }
+}
