823bc33b6d8548dcf057c2283b4a92e3f7a130ba
rohit ranjan <ranjan.rohit64@gmail.com>
2018-03-31 01:07:27 +0530

    Subtaction feature is also included

--- app/src/main/java/com/example/rohitranjan/logfiletest/MainActivity.java
+++ app/src/main/java/com/example/rohitranjan/logfiletest/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +25,16 @@ public class MainActivity extends AppCompatActivity {
         int sum = num1  + num2;
         e3.setText(Integer.toString(sum));
 
+    }
+
+    public void Subtract(View v)
+    {
+        EditText e1=(EditText) findViewById(R.id.editText);
+        EditText e2=(EditText) findViewById(R.id.editText2);
+        EditText e3=(EditText)findViewById(R.id.editText3);
+        int num1 = Integer.parseInt(e1.getText().toString());
+        int num2 = Integer.parseInt(e2.getText().toString());
+        int subtract = num1-num2;
+        e3.setText(Integer.toString(subtract));
     }
 }
